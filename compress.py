d00e7e41df5fbfd723e4bae03a60781408f601ff
Loïc <36301183+ftLoic@users.noreply.github.com>
2022-07-10 15:15:41 +0200

    new previews

--- compress.py
+++ compress.py
@@ -5,8 +5,13 @@ for path, subdirs, files in os.walk('img'):
     for name in files:
         if name[-4:] == ".png":
             filename = os.path.join(path, name)
-            new = filename.replace('.png', '.jpg')
 
-            img = cv2.imread(filename)
-            # img = cv2.resize(img, (854, 480))
-            cv2.imwrite(new, img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+            new = filename.replace('.png', '.jpg')
+            prev = filename.replace('.png', '_preview.jpg')
+
+            new_img = cv2.imread(filename)
+            prev_img = cv2.imread(filename)
+            prev_img = cv2.resize(prev_img, (854, 480))
+
+            cv2.imwrite(new, new_img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+            cv2.imwrite(prev, prev_img, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
